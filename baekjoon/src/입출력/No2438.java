a591272860fe8d620ac4f492a578b310448d4a27
2ezeeni <dpwls6787@gmail.com>
2021-03-15 00:30:50 +0900

    Add solution of 2438

--- baekjoon/src/입출력/No2438.java
+++ baekjoon/src/입출력/No2438.java
@@ -1,21 +1,25 @@
 package 입출력;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class No2438 {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
 		
 		for(int i = 0; i < n; i++) {
 			sb.append("*");
-			System.out.println(sb);
+			bw.write(sb + "\n");
 		}
+		bw.flush();
 		
 	}
 

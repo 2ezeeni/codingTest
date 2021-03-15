d1ff14934b2b38b81de3a287173d501ca8a9de41
2ezeeni <dpwls6787@gmail.com>
2021-03-15 23:35:04 +0900

    Add solution of 2439

---
+++ baekjoon/src/입출력/No2439.java
@@ -0,0 +1,40 @@
+package 입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No2439 {
+
+	public static void main(String[] args) throws IOException {
+		
+		//문제. 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+		//      오른쪽 정렬한 별을 출력하시오.
+		
+		// 출력 한줄씩 보면 공백과 별로 이루어져있다.
+		// 공백 : n-1부터 0까지 하나씩 줄어듬
+		// 별 : 1부터 n까지 하나씩 늘어남
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int num = Integer.parseInt(br.readLine());
+		
+		int blank = (num - 1);
+		int star = 1; 
+		
+		while(blank >= 0 && star <= num) {
+			for(int i = 0; i < blank; i++) {
+				sb.append(" ");
+			}
+			for(int j = 0; j < star; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+			blank--;
+			star++;
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}

d51e9f4c6709cfee8549b97118ec986c3e529a0e
2ezeeni <dpwls6787@gmail.com>
2021-03-17 20:38:47 +0900

    Update 1000, 2558

--- baekjoon/src/입출력/No2558.java
+++ baekjoon/src/입출력/No2558.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 public class No2558 {
 
 	public static void main(String[] args) throws IOException {
+		// 문제. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());

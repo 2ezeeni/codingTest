f7783762a7670f1df0a81681f908fe1f96e33f5c
2ezeeni <dpwls6787@gmail.com>
2021-03-20 23:55:43 +0900

    Add solution of 11719

---
+++ baekjoon/src/입출력/No11719.java
@@ -0,0 +1,18 @@
+package 입출력;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class No11719 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+		
+	}
+
+}

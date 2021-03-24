41bfb0077900c1b9cb621ff5f58caec7d22240aa
2ezeeni <dpwls6787@gmail.com>
2021-03-24 16:04:13 +0900

    Update 10991

--- baekjoon/src/입출력/No10991.java
+++ baekjoon/src/입출력/No10991.java
@@ -1,38 +1,37 @@
 package 입출력;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class No10991 {
 
 	public static void main(String[] args) throws IOException {
-		
 		// 문제. 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
 
+		// 맨 왼쪽 빈칸을 먼저 출력한 후
+		// 다음 칸부터 순서상 짝수일 때는 빈칸, 홀수일 때는 *을 출력하게 한다.
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int num = Integer.parseInt(br.readLine());
-
-	  for (int i = 1; i <= num; i++)
-       {
-           for (int j = i; j <= num - 1; j++)
-           {
-               System.out.print(" ");
-           }
-           for (int j = 1; j <= i*2-1; j++)
-           {
-               if (j % 2 == 1)
-                   System.out.print("*");
-               else
-                   System.out.print(" ");
-           }
-           System.out.println();
-      }
-
-
+		
+		for(int i = 1; i <= num; i++) { 
+			for(int blank = 0; blank < num - i; blank++) {//맨 왼쪽 빈칸 출력
+				sb.append(" ");
+			}
+			for(int j = 1; j <= 2 * i - 1; j++) { 
+				if(j % 2 == 1) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
 	}
 
 }
